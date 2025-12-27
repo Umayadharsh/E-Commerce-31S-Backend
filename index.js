5a27e093913fd55540e442fce2c0e6005e86f773
Umaya <umayadharsh@gmail.com>
2025-12-27 12:52:03 +0530

    Cors public

--- index.js
+++ index.js
@@ -26,17 +26,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      // allow Postman / server-to-server
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
-    credentials: true,
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "token"],
   })
